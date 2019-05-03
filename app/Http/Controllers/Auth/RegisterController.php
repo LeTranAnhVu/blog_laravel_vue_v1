0829024b8d2fc4d186702b144904880f4a9b371f
LeTranAnhVu <vule5726@gmail.com>
2019-05-04 00:42:18 +0700

    authorization fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Carbon\Carbon;
 
 class RegisterController extends Controller
 {
@@ -67,6 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'api_token' => Hash::make($data['email'].'-'.Carbon::now()->__toString())
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::group(['prefix'=>'posts'],function(){
-   Route::post('/store', 'PostController@store')->middleware(['auth']);
-
+Route::middleware('auth:api')->prefix('posts')->group(function(){
+   Route::post('/store', 'PostController@store');
 });
